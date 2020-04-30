6e9c75489e9eecf050c9867d38ff0127cb198327
Ekzaton <ekzaton@gmail.com>
2020-04-30 11:07:57 +0500

    Добавил функцию для обновления данных

--- src/controllers/task.js
+++ src/controllers/task.js
@@ -7,8 +7,9 @@ import {render, replace, RenderPosition} from "../utils/render.js";
 
 // Контроллер задачи
 export default class TaskController {
-  constructor(container) {
+  constructor(container, onDataChange) {
     this._container = container;
+    this._onDataChange = onDataChange;
 
     this._taskComponent = null;
     this._taskEditComponent = null;
@@ -26,9 +27,15 @@ export default class TaskController {
     });
 
     this._taskComponent.setArchiveButtonClickHandler(() => {
+      this._onDataChange(this, task, Object.assign({}, task, {
+        isArchive: !task.isArchive,
+      }));
     });
 
     this._taskComponent.setFavoritesButtonClickHandler(() => {
+      this._onDataChange(this, task, Object.assign({}, task, {
+        isFavorite: !task.isFavorite,
+      }));
     });
 
     this._taskEditComponent.setSubmitHandler((evt) => {
